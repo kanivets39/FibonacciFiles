dd738d25479d44f3b9251f5b679006ba916a07a1
Kanivets <kanivets39@gmail.com>
2020-11-27 07:15:03 +0200

    FibonacciSequenceGenerator logic implementation

---
+++ src/main/java/com/kanivets/fibonacci_files/service/FibonacciSequenceGenerator.java
@@ -0,0 +1,38 @@
+package com.kanivets.fibonacci_files.service;
+
+import com.kanivets.fibonacci_files.exception.FibonacciException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Fibonacci sequence generator.
+ */
+public class FibonacciSequenceGenerator {
+
+    /**
+     * Gets Fibonacci sequence List up to maximum permitted value.
+     *
+     * @param max the max value of sequence
+     * @return the Fibonacci sequence
+     * @throws FibonacciException the fibonacci exception
+     */
+    public List<Integer> getSequenceUpTo(int max) throws FibonacciException {
+        List<Integer> fibonacci = Stream.of(0, 1).collect(Collectors.toList());
+        int currentMax = 0;
+
+        if (max < 0) {
+            throw new FibonacciException("The maximum value must be greater than 0");
+        } else {
+            while (currentMax < max) {
+                currentMax = fibonacci.get(fibonacci.size() - 2) + fibonacci.get(fibonacci.size() - 1);
+                if (currentMax < max) {
+                    fibonacci.add(currentMax);
+                }
+            }
+        }
+
+        return fibonacci;
+    }
+}
